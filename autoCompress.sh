dfc771d71d9bec1ca670301feb641c2508bcd4bf
Juan Pablo Prieto <juanpabloprieto23@gmail.com>
2024-12-08 22:53:56 -0500

    Add the logic to create the deafult folders

--- autoCompress.sh
+++ autoCompress.sh
@@ -11,17 +11,19 @@ GENERAL_INDEX=3
 createFolder(){
 
     local PATH_INTERN="$PATH_DESTINY/$1"
-    #echo "PATH FINAL $PATH_INTER"
-    if [ -f "$PATH_INTERN" ]; then
+    #echo "PATH FINAL $PATH_INTERN"
+    if [ -d "$PATH_INTERN" ]; then
         echo "Path $1 exist ..."
     else
         echo "Path does not found"
         mkdir "$PATH_INTERN"
     fi
-
+    return
 }
 
 for i in ${GENERAL_FORLDERS[@]}
 do
     createFolder "$i"
 done
+
+#Organized the files depending of the extensions.
